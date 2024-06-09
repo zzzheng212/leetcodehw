01e85fb5fa60f5f0c741a468c0e5fbe3404f035f
zzzheng212 <125420902+zzzheng212@users.noreply.github.com>
2024-06-09 15:54:52 +0800

    Add files via upload

---
+++ leetcode/lc8.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
